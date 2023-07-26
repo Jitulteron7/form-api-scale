dca6b4fa6cf7eb92cf3c7c2ca0b3852504e63192
Jitulteron7 <jitulteron9@gmail.com>
2023-07-26 20:53:47 +0530

    docker-compose added

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ WORKDIR /app
 COPY package.json .
 RUN npm install 
 COPY . ./
-EXPOSE 8080
+ENV PORT 8082
+EXPOSE $PORT
 
 CMD [ "npm","run", "dev"]
 
--- src/config/const/server.js
+++ src/config/const/server.js
@@ -1,3 +1,3 @@
 const NAMESPACE = 'SERVER';
-const SERVER_PORT = process.env.SERVER_PORT || 8080;
+const SERVER_PORT = 8080;
 module.exports = { NAMESPACE, SERVER_PORT };
